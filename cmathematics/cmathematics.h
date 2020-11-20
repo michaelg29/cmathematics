a064d382fcf3a1978098eaec41c5fdaa5c57e16d
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2020-11-20 00:41:46 -0500

    Added missing documentation

--- cmathematics/cmathematics.h
+++ cmathematics/cmathematics.h
@@ -1,8 +1,11 @@
 typedef unsigned char bool;
 #define false 0
-#define true 1
+#define true !false
 
 /**
- * 
+ * calculates the number of arguments in a __VA_ARGS__ list
+ * @param type the type of the argument in the list
+ * @param ... the list
+ * @return the number of arguments in the list
  */
 #define NUMARGS(type, ...) (sizeof((type[]){0.0f, ##__VA_ARGS__}) / sizeof(type) - 1)
