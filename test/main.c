f01c4ca0bc0bbf911877622662940babc5cd3a69
michaelg29 <michaelgriecocoding@gmail.com>
2020-11-26 12:32:00 -0500

    Updated test file

--- test/main.c
+++ test/main.c
@@ -8,18 +8,16 @@ int main()
 {
     printf("Hello, world!\n");
 
-    mat m1 = newMatrix(2, 3,
-                       2.0f, 1.0f, 4.0f,
-                       0.0f, 1.0f, 1.0f);
+    mat m1 = matrix(2, 3,
+                    2.0f, 1.0f, 4.0f,
+                    0.0f, 1.0f, 1.0f);
     printMat(m1);
 
-    mat m2 = newMatrix(3, 4,
-                       6.0f, 3.0f, -1.0f, 0.0f,
-                       1.0f, 1.0f, 0.0, 4.0f,
-                       -2.0f, 5.0f, 0.0f, 2.0f);
+    mat m2 = matrix(3, 4,
+                    6.0f, 3.0f, -1.0f, 0.0f,
+                    1.0f, 1.0f, 0.0, 4.0f,
+                    -2.0f, 5.0f, 0.0f, 2.0f);
     printMat(m2);
-
-    printMat(matMatMultiplication(m1, m2));
 
     return 0;
 }
