41c35fb3959b01e20e34d94546793532b06ebcf7
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2021-09-10 10:14:34 -0400

    Added hexadecimal utility methods

--- cmathematics/cmathematics.h
+++ cmathematics/cmathematics.h
@@ -5,6 +5,9 @@ typedef unsigned char bool;
 #define false 0
 #define true !false
 
+// array containing hexadecimal digits in order
+extern char hex[16];
+
 /**
  * calculates the number of arguments in a __VA_ARGS__ list
  * @param type the type of the argument in the list
@@ -97,5 +100,24 @@ unsigned int numDigits(unsigned int val, unsigned int base);
  * @return the number of digits
  */
 unsigned long long numDigitsLL(unsigned long long val, unsigned long long base);
+
+/**
+ * method to scan a string in hexadecimal format and return its byte array representation
+ * @param str the hexadecimal string
+ * @param bytes the number of bytes in the output
+ * @return the byte array with the hexadecimal data
+ */
+unsigned char *scanHex(char *str, int bytes);
+
+/**
+ * method to output a byte array as a hexadecimal string
+ * @param arr the byte array
+ * @param noBytes the number of bytes
+ * @param delim word delimiter
+ * @param delimN the size of the delimiter
+ * @param wordSize number of bytes in a word
+ * @return the formatted string
+ */
+unsigned char *printByteArr(unsigned char *arr, int noBytes, unsigned char *delim, int delimN, int wordSize);
 
 #endif
--- test/main.c
+++ test/main.c
@@ -6,67 +6,23 @@
 #include "../cmathematics/sha3.h"
 #include "../cmathematics/sha1.h"
 
-char hex[16] = "0123456789ABCDEF";
-void printCharArr(unsigned char *arr, int len, bool hasSpace)
-{
-    printf("{ ");
-    for (int i = 0; i < len; i++)
-    {
-        printf("%c%c%s", hex[arr[i] >> 4], hex[arr[i] & 0x0f], hasSpace ? " " : "");
-    }
-    printf("%s}\n", hasSpace ? "" : " ");
-}
-
-unsigned char *scanHex(char *str, int n)
-{
-    int bytes = n >> 1;
-    unsigned char *ret = malloc(bytes);
-    memset(ret, 0, bytes);
-
-    for (int i = 0, i2 = 0; i < bytes; i++, i2 += 2)
-    {
-        // get value
-        for (int j = 0; j < 2; j++)
-        {
-            ret[i] <<= 4;
-            unsigned char c = str[i2 + j];
-            if (c >= '0' && c <= '9')
-            {
-                ret[i] += c - '0';
-            }
-            else if (c >= 'a' && c <= 'f')
-            {
-                ret[i] += c - 'a' + 10;
-            }
-            else if (c >= 'A' && c <= 'F')
-            {
-                ret[i] += c - 'A' + 10;
-            }
-            else
-            {
-                free(ret);
-                return NULL;
-            }
-        }
-    }
-
-    return ret;
-}
-
 int main()
 {
     printf("Hello, world!\n");
 
     unsigned char *msg = NULL;
     unsigned char *hash = NULL;
+    unsigned char *out = NULL;
 
-    const int N = 352;
+    const int N = 176;
     msg = scanHex("1F42ADD25C0A80A4C82AAE3A0E302ABF9261DCA7E7884FD869D96ED4CE88AAAA25304D2D79E1FA5CC1FA2C95899229BC87431AD06DA524F2140E70BD0536E9685EE78089590A012384709182357091283479AB93489182CD7397F12B261DCA7E7884FD869D96ED4CE88A261DCA7E7884FD869D96ED4CE88A261DCA7E7884FD869D96ED4CE88A261DCA7E7884FD869D96ED4CE88A261DCA7E7884FD869D96ED4CE88A261DCA7E7884FD869D96ED4CE88A", N);
     if (msg)
     {
-        int len = sha3_hash(msg, N >> 1, SHA3_256, &hash);
+        int len = sha3_hash(msg, 176, SHA3_256, &hash);
         printf("%d\n", len);
-        printCharArr(hash, len, false);
+        out = printByteArr(hash, len, NULL, 0, 0);
+        printf("%s\n", out);
+        free(out);
         free(hash);
     }
     else
@@ -85,7 +41,9 @@ int main()
     }
     sha1_digest(&ctx, &hash);
 
-    printCharArr(hash, SHA1_OUT, false);
+    out = printByteArr(hash, SHA1_OUT, " ", 1, 2);
+    printf("%s\n", out);
+    free(out);
 
     free(msg);
     free(hash);
